ebcd512f97b649650b8f2331a494fd0bd901d2bf
ngld <ngld@tproxy.de>
2019-08-20 16:11:02 +0200

    Fixed a race condition in the OverlayPluginApi handling

--- ui/raidboss/popup-text.js
+++ ui/raidboss/popup-text.js
@@ -548,13 +548,7 @@ addOverlayListener('onLogEvent', function(e) {
   gPopupText.OnLog(e);
 });
 
-if (window.callOverlayHandler) {
-  callOverlayHandler({
-    call: 'cactbotReadDataFiles',
-    source: location.href,
-  }).then((e) => gPopupText.OnDataFilesRead(e));
-} else {
-  addOverlayListener('onDataFilesRead', function(e) {
-    gPopupText.OnDataFilesRead(e);
-  });
-}
+callOverlayHandler({
+  call: 'cactbotReadDataFiles',
+  source: location.href,
+}).then((e) => gPopupText.OnDataFilesRead(e));
